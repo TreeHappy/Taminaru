8b3b212ac5b617714577cf58937e794da955a612
TreeHappy <97783479+TreeHappy@users.noreply.github.com>
2025-02-02 17:32:30 +0100

    update

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -3,17 +3,21 @@ $env:EDITOR = "nvim"
 $env:YAZI_FILE_ONE = "C:\Program Files\Git\usr\bin\file.exe"
 $env:YAZI_CONFIG_HOME = Join-Path $env:HOME ".config/yazi/"
 $env:XDG_CONFIG_HOME = Join-Path $env:HOME ".config"
+$env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense' # optional
+$env:LS_COLORS = "$(vivid generate catppuccin-frappe)"
+$env:GIT_EXTERNAL_DIFF = "difft"
 
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-Invoke-Expression (&starship init powershell)
-
-$env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense'
 Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 carapace _carapace | Out-String | Invoke-Expression
 
-$env:LS_COLORS="$(vivid generate catppuccin-frappe)"
+function rep
+{
+  . $PROFILE
+  Clear-Host
+}
 
 Set-Alias ls eza
 Set-Alias cat bat
-
+Set-Alias zoxide zoxide.exe
+Set-Alias git git.exe
