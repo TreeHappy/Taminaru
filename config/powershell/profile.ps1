389ac57d3cb7eff05b4482c6d410a1bf7ced8fd5
TreeHappy <97783479+TreeHappy@users.noreply.github.com>
2025-02-03 18:17:24 +0100

    update

--- config/powershell/profile.ps1
+++ config/powershell/profile.ps1
@@ -3,13 +3,18 @@ $env:EDITOR = "nvim"
 $env:YAZI_FILE_ONE = "C:\Program Files\Git\usr\bin\file.exe"
 $env:YAZI_CONFIG_HOME = Join-Path $env:HOME ".config/yazi/"
 $env:XDG_CONFIG_HOME = Join-Path $env:HOME ".config"
-$env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense' # optional
+$env:CARAPACE_BRIDGES = 'zsh,fish,bash,inshellisense'
 $env:LS_COLORS = "$(vivid generate catppuccin-frappe)"
 $env:GIT_EXTERNAL_DIFF = "difft"
+$env:STARSHIP_CONFIG = "${env:USERPROFILE}\.config\starship\starship.toml"
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 carapace _carapace | Out-String | Invoke-Expression
+
+Invoke-Expression (&starship init powershell)
 
 function rep
 {
@@ -21,3 +26,5 @@ Set-Alias ls eza
 Set-Alias cat bat
 Set-Alias zoxide zoxide.exe
 Set-Alias git git.exe
+Set-Alias docker docker.exe
+Set-Alias chezmoi chezmoi.exe
